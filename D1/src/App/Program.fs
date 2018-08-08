3a5dc07ce4d4fd8b0fd5e75433ca6ddae0445343
Stephen Price <stephen@lythixdesigns.com>
2018-08-08 21:12:04 +0800

    Added D1 and D2

---
+++ D1/src/App/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+// open System
+
+// [<EntryPoint>]
+// let main argv =
+//    printfn "Hello World from F#!"
+//    0 // return an integer exit code
+
+open System
+open Library
+
+[<EntryPoint>]
+let main argv =
+    printfn "Nice command-line arguments! Here's what JSON.NET has to say about them:"
+
+    argv
+    |> Array.map getJsonNetJson
+    |> Array.iter (printfn "%s")
+
+    0 // return an integer exit code
---
+++ D2/D2/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open BasicFunctions
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let result2 = sampleFunction2 (7 + 4)
+    printfn "The result of applying the 2nd sample function to (7 + 4) is %d" result2
+
+    0 // return an integer exit code
